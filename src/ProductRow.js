bacdffe8eef97cc384f30cdfa658e47bb554b07d
ajorve <anders@codeinfusion.com>
2017-05-08 19:08:35 -0700

    Adding eslinter module

--- src/ProductRow.js
+++ src/ProductRow.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
 export default class ProductRow extends React.Component {
-    render() {
-        let name = this.props.product.stocked ?
-            this.props.product.name :
-            <span style={{color: 'red'}}>
-        {this.props.product.name}
-      </span>;
-        return (
-            <tr>
-                <td>{name}</td>
-                <td>{this.props.product.price}</td>
-            </tr>
-        );
-    }
+  render () {
+    let name = this.props.product.stocked
+            ? this.props.product.name
+            : <span style={{color: 'red'}}>
+              {this.props.product.name}
+            </span>
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    )
+  }
 }
